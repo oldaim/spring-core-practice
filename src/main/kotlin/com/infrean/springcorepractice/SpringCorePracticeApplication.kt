caf556ce25b9bc401dcd3f8bab640d2f101c1da5
Kimdongkyun <dgk0911@gmail.com>
2023-03-15 22:11:14 +0900

    인프런 강의 연습 프로젝트 초기설정

---
+++ src/main/kotlin/com/infrean/springcorepractice/SpringCorePracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.infrean.springcorepractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCorePracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCorePracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/infrean/springcorepractice/SpringCorePracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.infrean.springcorepractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCorePracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
