aa2ea6ae7edcd0ced24b959bb2ba7e8c9912a51d
Kimdongkyun <dgk0911@gmail.com>
2023-04-04 21:16:09 +0900

    [PRC-2] 4/4 강의 완료 - ApplicationContext test

--- src/test/kotlin/com/infrean/springcorepractice/SpringCorePracticeApplicationTests.kt
+++ src/test/kotlin/com/infrean/springcorepractice/SpringCorePracticeApplicationTests.kt
@@ -1,13 +1,46 @@
 package com.infrean.springcorepractice
 
+import com.infrean.springcorepractice.discount.DiscountPolicy
+import com.infrean.springcorepractice.discount.FixDiscountPolicy
+import com.infrean.springcorepractice.discount.RateDiscountPolicy
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootTest
 class SpringCorePracticeApplicationTests {
 
-    @Test
-    fun contextLoads() {
+   val ac = AnnotationConfigApplicationContext(TestConfig::class.java)
+
+    @Configuration
+    class TestConfig{
+
+        @Bean
+        fun rateDiscountPolicy(): DiscountPolicy = RateDiscountPolicy()
+
+        @Bean
+        fun fixDiscountPolicy(): DiscountPolicy = FixDiscountPolicy()
+
     }
+
+    @Test
+    fun findAllBeanByParentType(){
+        val beansOfType = ac.getBeansOfType(DiscountPolicy::class.java)
+        assertThat(beansOfType.size).isEqualTo(2)
+    }
+
+    @Test
+    fun findAllBeanByObjectType(){
+        val beansOfType = ac.getBeansOfType(Any::class.java)
+        beansOfType.keys.forEach{
+            println(it)
+        }
+
+    }
+
 
 }
